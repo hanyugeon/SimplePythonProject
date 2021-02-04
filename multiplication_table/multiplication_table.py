e9ff98eada8b2f012a397858ddf86dc8f0572ea2
hanyugeon <hanyugeon@naver.com>
2021-02-04 22:52:27 +0900

    multiplication_cable_01

---
+++ multiplication_table/multiplication_table.py
@@ -0,0 +1,68 @@
+# 구구단 결과를 불러오는 함수를 적는다.
+# import를 활용하여 index.py 에 호출시킬것.
+# 함수는 총 9가지가 될 것이다. (1 부터 9 까지)
+# 빠르게 만들고 더 효율적인 방법이 있는지 고려해보기
+
+def one(number):
+    print('%d 단 출력.' % (number))
+    for i in range(1, 10):
+        print('%d x %d' % (number, i))
+
+
+def two(number):
+    print('%d 단 출력.' % (number))
+    for i in range(1, 10):
+        print('%d x %d' % (number, i))
+
+
+def three(number):
+    print('%d 단 출력.' % (number))
+    for i in range(1, 10):
+        print('%d x %d' % (number, i))
+
+
+def four(number):
+    print('%d 단 출력.' % (number))
+    for i in range(1, 10):
+        print('%d x %d' % (number, i))
+
+
+def five(number):
+    print('%d 단 출력.' % (number))
+    for i in range(1, 10):
+        print('%d x %d' % (number, i))
+
+
+def six(number):
+    print('%d 단 출력.' % (number))
+    for i in range(1, 10):
+        print('%d x %d' % (number, i))
+
+
+def seven(number):
+    print('%d 단 출력.' % (number))
+    for i in range(1, 10):
+        print('%d x %d' % (number, i))
+
+
+def eight(number):
+    print('%d 단 출력.' % (number))
+    for i in range(1, 10):
+        print('%d x %d' % (number, i))
+
+
+def nine(number):
+    print('%d 단 출력.' % (number))
+    for i in range(1, 10):
+        print('%d x %d' % (number, i))
+
+# def main():
+#     print("Testing mt.one function")
+#
+#     test_number = 1
+#     one(test_number)
+#
+#     print("test is end!")
+#
+# if __name__='__name__':
+#     main()
