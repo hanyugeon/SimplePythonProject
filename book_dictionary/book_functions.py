e50729c50d9b3a7fa47403f7a508741637a71a6b
hanyugeon <hanyugeon@naver.com>
2021-02-07 18:39:01 +0900

    commit_book_dict_01

---
+++ book_dictionary/book_functions.py
@@ -0,0 +1,15 @@
+# 조건부로 책 찾기(함수)
+# 01. 감명도 높은 순(중복시 이름순으로)
+# 02. 추천 유무(중복시 감명도 높은 순으로 정렬)
+# 03. 책 이름순 나열
+
+# dict 자료 순서 : 제목(str), 읽은 순서(1 ~  / int), 감명도(0 ~ 5 / int), 추천유무(o, x / str)
+
+def impression_level(book_list):
+
+
+def recommended_list(book_list):
+
+
+def list_by_name(book_list):
+
